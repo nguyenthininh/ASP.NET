7aa3eda3105f281638303691d376e9d4a6dd9827
Nina Nguyen <Ninh2000md>
2020-09-12 19:01:51 +0700

    HelloMVC

---
+++ date2/HelloMVC/HelloMVC/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HelloMVC.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Prince { get; set; }
+
+    }
+    public class MovieDBContext : DbContext    //kết nối đến CSDL thông qua MovieDBContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
